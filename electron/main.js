22700f4f0a54ead58613e9a405d3d7c2a0203d99
John Pade <jaune@Johns-MacBook-Pro.local>
2023-12-01 15:33:19 +0100

    Etape 1 à 3 du TP PADE John

--- electron/main.js
+++ electron/main.js
@@ -1,10 +1,13 @@
-const { app, BrowserView, BrowserWindow, ipcMain} = require('electron');
-const{initEventsHandler} = require('./handleEvents');
+const { app, BrowserWindow, BrowserView, ipcMain } = require('electron');
+const { initEventsHandler } = require('./handleEvents');
 const isDev = require("electron-is-dev");
 const path = require("path");
 
+let browserWindow;
+let browserView;
+
 app.whenReady().then(() => {
-    const browserWindow = new BrowserWindow({
+    browserWindow = new BrowserWindow({
         width: 800,
         height: 800, 
         webPreferences: {
@@ -16,23 +19,35 @@ app.whenReady().then(() => {
 
     if (isDev){
         browserWindow.loadURL("http://localhost:4200/");
-    }else {
+    } else {
         browserWindow.loadFile('dist/browser/index.html');
     }
 
-    const browserView = new BrowserView();
+    browserView = new BrowserView();
     initEventsHandler(browserWindow, browserView);
     
-    browserWindow.once('ready-to-show', ()=>{
-        browserWindow.setBrowserView(browserView)
-        const winSize = browserWindow.webContents.getOwnerBrowserWindow().getBounds();
-        browserView.setBounds({ x: 0, y: 55, width: winSize.width, height: winSize.height });
+    browserWindow.once('ready-to-show', () => {
+        browserWindow.setBrowserView(browserView);
+        const winSize = browserWindow.getBounds();
+        browserView.setBounds({ x: 0, y: 55, width: winSize.width, height: winSize.height - 55 });
         browserView.webContents.loadURL('https://amiens.unilasalle.fr');
     });
     
-    browserWindow.on('resized', ()=>{
-        const winSize = browserWindow.webContents.getOwnerBrowserWindow().getBounds();
-        browserView.setBounds({ x: 0, y: 55, width: winSize.width, height: winSize.height });
+    browserWindow.on('resized', () => {
+        const winSize = browserWindow.getBounds();
+        browserView.setBounds({ x: 0, y: 55, width: winSize.width, height: winSize.height - 55 });
     });
-})
 
+    // Ajouter l'écouteur d'événements IPC pour la capture d'écran
+    ipcMain.on('capture-page', async (event) => {
+        try {
+            const image = await browserView.webContents.capturePage();
+            const buffer = image.toPNG(); // Convertit en PNG
+            event.reply('capture-page-reply', buffer);
+        } catch (error) {
+            console.error('Erreur lors de la capture de la page :', error);
+        }
+    });
+});
+
+// ... le reste de votre code
--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+declare const window: any;
+const ipcRenderer = window.require('electron').ipcRenderer;
+
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
@@ -7,4 +10,32 @@ import { Component } from '@angular/core';
 })
 export class ToolbarComponent {
 
+  captureScreen() {
+    ipcRenderer.send('capture-page');
+  }
+
+  downloadImage(url: string) {
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'captured-image.png'; // Nom du fichier à télécharger
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+  
+    // Libérer l'URL du Blob après le téléchargement
+    URL.revokeObjectURL(url);
+  }
+  
+
+  ngOnInit() {
+    ipcRenderer.on('capture-page-reply', (event: any, buffer: BlobPart) => {
+      // Traiter l'image reçue, par exemple l'afficher ou la sauvegarder
+      // Par exemple, convertir le buffer en Blob et créer un URL pour l'afficher dans un élément <img>
+      const blob = new Blob([buffer], { type: 'image/png' });
+      const url = URL.createObjectURL(blob);
+      // Appeler la fonction pour télécharger l'image
+      this.downloadImage(url);
+    });
+  }
+ 
 }
